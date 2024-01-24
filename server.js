380020d23729350eb64cbf96464e7cf3920d0512
kjirstenhernandez <122486287+kjirstenhernandez@users.noreply.github.com>
2024-01-24 13:29:13 -0700

    Finished MVC Setup

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* *************************
  * View Engine and Templates
@@ -23,6 +24,7 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
+app.use("/inv", inventoryRoute)
 
 // Index route
 app.get("/", baseController.buildHome)
