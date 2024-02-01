22c91b05e4e768d61bad310ddd7f8bad11e69f14
kjirstenhernandez <122486287+kjirstenhernandez@users.noreply.github.com>
2024-01-31 22:10:23 -0700

    Completed Registration Page

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -6,5 +6,6 @@ const accountController = require("../controllers/accountController")
 
 // 
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
+router.get("/register", utilities.handleErrors(accountController.buildRegistration))
 
 module.exports = router;
