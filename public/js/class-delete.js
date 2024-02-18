3aec31b79091a151af55250f8ce0cca37f87de3c
kjirstenhernandez <122486287+kjirstenhernandez@users.noreply.github.com>
2024-02-17 20:50:42 -0700

    Housekeeping

--- public/js/class-delete.js
+++ public/js/class-delete.js
@@ -4,16 +4,10 @@
  // Get a list of items in inventory based on the classification_id 
 let classificationList = document.querySelector("#classificationList")
 let deleteButton = document.querySelector("#deletebutton")
-console.log("I'm getting this")
+
 classificationList.addEventListener("change", function () { 
   let classification_id = classificationList.value 
-  console.log(`classification_id is: ${classification_id}`)
   deleteButton.innerHTML = `<a href="/inv/delete-class/${classification_id}">Delete</a>`
   
-  
-  // const link = document.createElement("a")
-  
-  
-  // link.setAttribute("href", `/inv/delete-class/${classification_id}`)
-  // deleteButton.appendChild(link)
+
 })
